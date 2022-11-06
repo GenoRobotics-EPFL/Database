e101551f700837ee1899fafe9eb5c0d70d93dc1e
melinacherchali <114920014+melinacherchali@users.noreply.github.com>
2022-11-07 00:12:58 +0100

    Melina (#15)
    Co-authored-by: ChrMelina <melina.cherchali@icloud.com>

--- interface/pages/index.tsx
+++ interface/pages/index.tsx
@@ -2,10 +2,10 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 
 import { AppShell, Navbar, Header, Title, Button, Center } from '@mantine/core'
+
 import { useState } from 'react'
 
 import Link from 'next/link'
-
 
 
 
@@ -14,15 +14,14 @@ const Home: NextPage = () => {
   const [clicked, setClicked] = useState(false)
 
 
-
-
   return (
     <>
       <Head>
         <title>Home</title>
         <meta name="description" content="Home page" />
         <link rel="icon" href="/favicon.ico" />
-        </Head>
+      </Head>
+
 
       <AppShell
         padding="md"
@@ -32,34 +31,38 @@ const Home: NextPage = () => {
               Menus
             </Title>
 
-            <Button 
-              type = "button"
-              variant="light" color="violet" size="xs" compact            
-              disabled={clicked}
-              onClick={() => setClicked(true)}
+            <Button
+              component="a"
+              href="/posts/new_method"
               sx={{ width: 200 }}
-              ><Link href="/posts/new_method">Add a new method</Link>
+            >Add a new method
             </Button><br />
 
-            <Button 
-              type = "button"
-              variant="light" color="violet" size="xs" compact            
-              disabled={clicked}
-              onClick={() => setClicked(true)}
-              sx={{ width: 200 }}
-              ><Link href="/posts/new_sample">Add a new sample</Link>
-            </Button>
+            <Button
+              component="a" sx={{ width: 200 }}
+              href="/posts/new_sample"
+            >Add a new sample
+            </Button><br />
+
+            <Button
+              component="a" sx={{ width: 200 }}
+              href="/posts/new_specimen"
+            >Add a new specimen
+            </Button><br />
 
 
           </Navbar>
         }
 
         header={
+          
           <Header height={60} p="xs">
+          
             <Title order={1}>
-              GenoRobotics 
+              GenoRobotics
             </Title>
           </Header>
+          
         }
         styles={(theme) => ({
           main: { backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0] },
@@ -69,28 +72,15 @@ const Home: NextPage = () => {
 
 
 
-    
-
-  
-
-          {/* <Center sx={{ height: "100%" }}>
-            <Button
-            disabled={clicked}
-            onClick={() => setClicked(true)}
-            sx={{ width: 100 }}
-            >
-            {clicked ?
-              "Nice" :
-              "Login"
-            }
-            </Button>
-          
-          </Center> */}
 
 
-            </AppShell>
-            </>
-            )
+
+
+
+
+      </AppShell>
+    </>
+  )
 }
 
 export default Home
