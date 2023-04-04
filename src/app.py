417ac0be7ada3b0181bfd32b2c565b57febda22b
Plouce314 <alexandre.k.goumaz@gmail.com>
2023-04-04 14:24:21 +0200

    add cors in app

--- src/app.py
+++ src/app.py
@@ -53,7 +53,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://genorobotics.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
